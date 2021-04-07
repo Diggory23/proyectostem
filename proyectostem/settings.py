a103731b03df6655818268d6f33b994e08019b3f
Diggory23 <62451200+Diggory23@users.noreply.github.com>
2021-04-07 10:36:12 -0500

    Update settings.py

--- proyectostem/settings.py
+++ proyectostem/settings.py
@@ -30,7 +30,7 @@ with open(BASE_DIR / 'SECRET_KEY') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -129,3 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
